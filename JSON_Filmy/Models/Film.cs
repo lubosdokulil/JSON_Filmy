5c3796cb0f9fd9c08ea21fb96b84e198b546979f
lubosdokulil <lubosdokulil6@gmail.com>
2026-03-01 13:48:05 +0100

    Přidejte soubory projektu.

---
+++ JSON_Filmy/Models/Film.cs
@@ -0,0 +1,11 @@
+﻿namespace JSON_Filmy.Models
+{
+    public class Film
+    {
+        // Názvy vlastností musí přesně odpovídat klíčům v JSON souboru
+        public int id { get; set; }
+        public string nazev { get; set; }
+        public int rokVydani { get; set; }
+        public string reziser { get; set; }
+    }
+}

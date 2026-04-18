f064639e35402a2fe3ca24d49814f01fbcedcfcd
lubosdokulil <lubosdokulil6@gmail.com>
2026-04-18 08:50:07 +0200

    zmena nazvu vlastnosti

--- JSON_Filmy/Models/Film.cs
+++ JSON_Filmy/Models/Film.cs
@@ -1,11 +1,20 @@
-﻿namespace JSON_Filmy.Models
+﻿using System.Text.Json.Serialization;
+namespace JSON_Filmy.Models
 {
     public class Film
     {
-        // Názvy vlastností musí přesně odpovídat klíčům v JSON souboru
-        public int id { get; set; }
-        public string nazev { get; set; }
-        public int rokVydani { get; set; }
-        public string reziser { get; set; }
+        // Vlastnosti dodržují C# konvence a jsou namapovány na JSON klíče
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("nazev")]
+        public string Nazev { get; set; }
+
+        [JsonPropertyName("rokVydani")]
+        public int RokVydani { get; set; }
+
+        [JsonPropertyName("reziser")]
+        public string Reziser { get; set; }
+
     }
 }
